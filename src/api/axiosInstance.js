ec718a5fc3c629b5f162ce9849c53e143c075337
Dafdaf <81273171+Dffarhn@users.noreply.github.com>
2025-06-22 11:43:01 +0700

    fix: update baseURL in axiosInstance to correct API endpoint

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://enormous-mint-tomcat.ngrok-free.app/api/v1",
+  baseURL: "https://noble-doe-strongly.ngrok-free.app/api/v1",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "69420",
